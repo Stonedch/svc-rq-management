3f5c8d93b3d14ee87067b41b94e7f21d2c813a28
Max Mukhin <stch.max.muhin@gmail.com>
2018-10-21 16:10:03 +0300

    chore: remove mustache, add thymeleaf

--- src/main/java/com/example/task/controller/MainController.java
+++ src/main/java/com/example/task/controller/MainController.java
@@ -1,16 +1,12 @@
 package com.example.task;
 
-import com.example.task.domain.Request;
-import com.example.task.domain.User;
 import com.example.task.repository.RequestRepository;
 import com.example.task.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
