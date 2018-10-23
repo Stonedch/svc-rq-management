c188087e1bb743a7d78bc90fb7ef9e4aa464223d
Max Mukhin <stch.max.muhin@gmail.com>
2018-10-23 05:52:15 +0300

    fix: binding exception

--- src/main/java/com/example/task/controller/ClientController.java
+++ src/main/java/com/example/task/controller/ClientController.java
@@ -2,10 +2,10 @@ package com.example.task.controller;
 
 import com.example.task.domain.Request;
 import com.example.task.repository.RequestRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,8 +22,9 @@ public class ClientController {
     }
 
     @PostMapping("/client")
-    public void submit(@ModelAttribute Request request, Model model) {
-        requestRepository.save(request);
+    public void submit(@ModelAttribute Request request,
+                       BindingResult bindingResult, Model model) {
+        if (!bindingResult.hasErrors()) requestRepository.save(request);
         getClient(model);
     }
 }
