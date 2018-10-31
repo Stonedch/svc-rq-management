1c44d34570c5466c73d71c0b55e8250559e2019f
Max Mukhin <stch.max.muhin@gmail.com>
2018-11-01 02:31:21 +0300

    feat: add post controller for admin (createRequest)

--- src/main/java/com/example/task/controller/AdminController.java
+++ src/main/java/com/example/task/controller/AdminController.java
@@ -3,19 +3,29 @@ package com.example.task.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.task.domain.Request;
 import com.example.task.repository.RequestRepository;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
     @Autowired
-    RequestRepository requestRepository;
+    private RequestRepository requestRepository;
 
     @GetMapping
     public String getAdmin(Model model) {
+        model.addAttribute(new Request());
         model.addAttribute("requests", requestRepository.findAll());
         return "admin";
+    }
+
+    @PostMapping("/request")
+    public String createRequest(@ModelAttribute Request request,
+                                BindingResult bindingResult) {
+        if (!bindingResult.hasErrors()) requestRepository.save(request);
+        return "redirect:/admin";
     }
 }
