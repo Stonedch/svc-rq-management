7771a57a385b83c5ea6a634296ee9038d751c996
Max Mukhin <stch.max.muhin@gmail.com>
2018-11-09 20:27:09 +0300

    chore: delete user entity and repository

--- src/main/java/com/github/stonedch/servicemanagement/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.stonedch.servicemanagement.repository;
-
-import com.github.stonedch.servicemanagement.domain.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long>{
-
-    User findByLoginAndPassword(String login, String password);
-}
