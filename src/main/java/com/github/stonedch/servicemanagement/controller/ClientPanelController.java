dc518a0d4078e16da712ed62a635d93896e02869
Max Mukhin <stch.max.muhin@gmail.com>
2018-11-09 18:42:56 +0300

    refactor: rename class, methods and template (Client Panel)

--- src/main/java/com/github/stonedch/servicemanagement/controller/ClientPanelController.java
+++ src/main/java/com/github/stonedch/servicemanagement/controller/ClientPanelController.java
@@ -1,7 +1,5 @@
 package com.github.stonedch.servicemanagement.controller;
 
-import com.github.stonedch.servicemanagement.domain.Request;
-import com.github.stonedch.servicemanagement.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,22 +8,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.github.stonedch.servicemanagement.domain.Request;
+import com.github.stonedch.servicemanagement.repository.RequestRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
-public class ClientController {
+@RequestMapping("/client-panel")
+public class ClientPanelController {
     @Autowired
     private RequestRepository requestRepository;
 
-    @GetMapping("/client")
-    public String getClient(Model model) {
-        model.addAttribute("request", new Request());
-        model.addAttribute("requests" , requestRepository.findAll());
-        return "client";
+    @GetMapping
+    public String client(Model model) {
+        model.addAttribute("newRequest", new Request());
+        model.addAttribute("requestList" , requestRepository.findAll());
+        return "client-panel";
     }
 
-    @PostMapping("/client")
+    @PostMapping
     public String submit(@ModelAttribute Request request,
                          BindingResult bindingResult) {
         if (!bindingResult.hasErrors()) requestRepository.save(request);
-        return "redirect:/client";
+        return "redirect:/client-panel";
     }
 }
